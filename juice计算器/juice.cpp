3b4df4e20438e41665c626d863ec1d83653f3fb6
Chuang1213 <75832567+chuang1213@users.noreply.github.com>
2020-12-19 03:47:30 +0800

    Add files via upload
    第一个C++项目，想了很久。
    语法还没学好，很生疏

---
+++ juice计算器/juice.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	long int way, n1, n2, o;
+	cout << "mathser v1.0.5 by C&J" << endl;
+	cout << "输入一个数字" << endl;
+	cin >> n1;
+	cout << "输入另一个数字" << endl;
+	do {
+		cin >> n2;
+		cout << "选择计算方式" << endl << "1+ 2- 3x 4÷" << endl;
+		cin >> way;
+
+		if (way == 1) {
+			o = n1 + n2;
+			cout << n1 << "+" << n2 << "=" << o<<endl;
+         }//加法
+
+		else if (way == 2) {
+			o = n1 - n2;
+			cout << n1 << "-" << n2 << "=" << o<<endl;
+		}//减法
+
+		else if (way == 3) {
+			o = n1 * n2;
+			cout << n1 << "x" << n2 << "=" << o<<endl;
+		}//除法
+
+		else if (way == 4) {
+			o = n1 / n2;
+			cout << n1 << "÷" << n2 << "=" << o << endl;
+		}//乘法
+
+		else {
+			cout << "choose the right way!";
+			break;
+		}//特殊情况
+
+		n1 = o;//下一次运算
+
+	} while (n1);
+}
